e9243b029b0bcc0132e49252597bfbba109f39d8
김종혁 <notify9637@naver.com>
2023-08-29 15:46:57 +0900

    230829 select 기본

--- sql연습/basic-query.sql
+++ sql연습/basic-query.sql
@@ -38,7 +38,33 @@ delete from pet where name='Doma';
 
 -- load data
 load data local infile 'd:\pet.txt' into table pet;
-
+update pet set death = null where name != 'bowser';
 -- select 연습
 -- 문) bowser 주인의 이름은?
 select owner from pet where name='bowser';
+
+-- 문2) 1998 이후에 태어난 애들은?
+select * from pet where birth >= '1998-01-01';
+
+-- 문3) 종이 뱀이거나 새인 애들은?
+select * from pet where species = 'snake' or species = 'bird';
+
+-- 문4) order by
+select name, birth from pet order by birth asc;
+
+-- 문5) order by ~desc
+select name, birth from pet order by birth desc;
+
+-- 문6) where절에 null 다루기
+select name, birth, death where death is null;
+
+-- 문7) like 검색(패턴 검색)
+select name from pet where name like 'b%';
+select name from pet where name like '%fy';
+select name from pet where name like '%W%';
+select name from pet where name like '____';
+select name from pet where name like 'b_____'; -- bowser
+
+-- 문8) 집계: count, avg, sum, max, min ...
+select count(*) from pet;
+select max(birth) from pet;
