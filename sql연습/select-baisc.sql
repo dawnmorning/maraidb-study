5fc522467c98eb5e3710a4b27279926d4faf3f48
김종혁 <notify9637@naver.com>
2023-08-29 17:07:04 +0900

    문제1, 문제2 완료

--- sql연습/select-baisc.sql
+++ sql연습/select-baisc.sql
@@ -20,10 +20,11 @@ select distinct title from titles;
 
 -- 문제1.
 -- 사번이 10944인 사원의 이름은(first_name)
-
+select first_name from employees where emp_no=10944;
 -- 문제2. 
 -- 전체직원의 다음 정보를 조회하세요. 가장 선임부터 출력이 되도록 하세요. 출력은 이름, 성별,  입사일 순서이고 “이름”, “성별”, “입사일로 컬럼 이름을 대체해 보세요.
-
+select * from employees;
+select first_name as '이름', gender as '성별', hire_date as '입사일' from employees; 
 -- 문제3.
 -- 여직원과 남직원은 각 각 몇 명이나 있나요?
 select count(*) from employees;
