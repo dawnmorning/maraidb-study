da1c222381a695401d94f598fd96be701e3a9306
김종혁 <notify9637@naver.com>
2023-08-31 17:33:19 +0900

    서브쿼리

--- sql연습/서브쿼리.sql
+++ sql연습/서브쿼리.sql
@@ -3,207 +3,185 @@
 --
 
 --
--- 1) select 절 : select(select...), insert into t value(...)
+-- 1) select절의 컬럼 프로젝션의 서브쿼리, insert into t values(서브쿼리)
 --
 
 --
--- 2) select의 from절의 서브쿼리
+-- 2) select의 from 절의 서브쿼리
 --
-
-select * 
-from (select now() as n, sysdate() as s, 3+1 as r from dual) a;
+select a.n, a.s, a.r
+  from (select now() as n, sysdate() as s, 3 + 1 as r from dual) a;
 
 --
--- 3) select의 where절 또는 having의 서브쿼리
---
+-- 3) select의 where절(having절)의 서브쿼리
+--  
+-- 예제: 현재, Fai Bale 이 근무하는 부서에서 근무하는 다름 직원의 사번, 이름을 출력
 
--- 예) 현재, Fai Bale이 근무하는 부서에서 근무하는 다른 직원의 사번, 이름을 출력
 -- sol1) 비추
-select demp.dept_no from employees e join dept_emp demp on e.emp_no = demp.emp_no where demp.to_date = '9999-01-01' and concat(first_name, ' ', last_name) = 'Fai Bale';
+select b.dept_no
+  from employees a, dept_emp b
+ where a.emp_no = b.emp_no
+   and b.to_date = '9999-01-01'
+   and concat(first_name, ' ', last_name) = 'Fai Bale';
 
-select e.empno, e.first_name from employees e join dept_emp demp on e.emp_no = demp.emp_no where demp.to_date = '9999-01-01' and concat(first_name, ' ', last_name) = 'Fai Bale';
+-- 'd004'
+
+select a.emp_no, a.first_name
+  from employees a, dept_emp b
+ where a.emp_no = b.emp_no
+   and b.to_date = '9999-01-01'
+   and b.dept_no = 'd004';  
 
 -- sol2) subquery 사용
-SELECT 
-    a.emp_no, a.first_name
-FROM
-    employees a,
-    dept_emp b
-WHERE
-    a.emp_no = b.emp_no
-        AND b.to_date = '9999-01-01'
-        AND dept_no = (SELECT 
-            b.dept_no
-        FROM
-            employees a,
-            dept_emp b
-        WHERE
-            a.emp_no = b.emp_no
-                AND b.to_date = '9999-01-01'
-                AND CONCAT(first_name, ' ', last_name) = 'Fai Bale');
-                
--- 3-1) 단일행 연산자 : =, > , <, >=, <=, <>. !=
--- 예재 : 현재 근무중인 전체 사원의 평균 연봉보다 적은 급여를 받는 사원의 이름과 급여
--- subquery 먼저 짜고 main query 짜기
+select a.emp_no, a.first_name
+  from employees a, dept_emp b
+ where a.emp_no = b.emp_no
+   and b.to_date = '9999-01-01'
+   and b.dept_no = (select b.dept_no
+					  from employees a, dept_emp b
+					 where a.emp_no = b.emp_no
+                       and b.to_date = '9999-01-01'
+                       and concat(first_name, ' ', last_name) = 'Fai Bale');
 
--- subquery
-select avg(salary) from salaries where to_date = '9999-01-01';
--- main query
-SELECT 
-    e.first_name,
-    s.salary
-FROM
-    employees e,
-    salaries s
-WHERE
-    e.emp_no = s.emp_no
-        AND s.to_date = '9999-01-01'
-        AND s.salary < (SELECT 
-            AVG(salary)
-        FROM
-            salaries
-        WHERE
-            to_date = '9999-01-01')
-	order by s.salary desc;
--- 예2) : 현재 근무중인 가장 적은 평균 급여의 직책과 그 급여를 출력
+-- 3-1) 단일행 연산자: =, >, <, >=, <=, <>, !=
+
+-- 예제1: 현재, 전체 사원의 평균 연봉보다 적은 급여를 받는 사원의 이름과 급여를 출력
+select avg(salary) from salaries where to_date='9999-01-01';
+
+  select a.first_name, b.salary
+    from employees a, salaries b
+   where a.emp_no = b.emp_no
+     and b.to_date = '9999-01-01'
+     and b.salary < (select avg(salary)
+                       from salaries
+					  where to_date='9999-01-01')
+order by b.salary desc;                    
+
+-- 예제2: 현재, 가장 적은 평균 급여의 직책과 그 급여를 출력
 -- Engineer 20000
--- subquery
-SELECT 
-    t.title, AVG(s.salary)
-FROM
-    titles t,
-    salaries s
-WHERE
-    t.emp_no = s.emp_no
-        AND t.to_date = '9999-01-01'
-        AND s.to_date = '9999-01-01'
-GROUP BY t.title
-having avg(s.salary) = (select min(avg_salary) from (SELECT 
-    avg(s.salary) as avg_salary
-FROM
-    salaries s,
-    titles t
-WHERE
-    s.emp_no = t.emp_no
-        AND s.to_date = '9999-01-01'
-        AND t.to_date = '9999-01-01'
-group by t.title) sub);
 
-
-select min(avg_salary) from (SELECT 
-    avg(s.salary) as avg_salary
-FROM
-    salaries s,
-    titles t
-WHERE
-    s.emp_no = t.emp_no
-        AND s.to_date = '9999-01-01'
-        AND t.to_date = '9999-01-01'
-group by t.title) sub;
+-- sol1) subquery
+  select a.title, avg(b.salary) as avg_salary
+    from titles a, salaries b
+   where a.emp_no = b.emp_no
+     and a.to_date = '9999-01-01'
+     and b.to_date = '9999-01-01'
+group by a.title
+  having avg_salary = (  select min(avg_salary)	
+						  from (select avg(b.salary) as avg_salary
+									from titles a, salaries b
+								   where a.emp_no = b.emp_no
+									 and a.to_date = '9999-01-01'
+									 and b.to_date = '9999-01-01'
+								group by a.title) a);
 
 -- sol2) top-k
-SELECT 
-    t.title, AVG(s.salary)
-FROM
-    titles t,
-    salaries s
-WHERE
-    t.emp_no = s.emp_no
-        AND t.to_date = '9999-01-01'
-        AND s.to_date = '9999-01-01'
-GROUP BY t.title
-order by avg(s.salary)
-limit 0, 1;
+   select a.title, avg(b.salary) as avg_salary
+    from titles a, salaries b
+   where a.emp_no = b.emp_no
+     and a.to_date = '9999-01-01'
+     and b.to_date = '9999-01-01'
+group by a.title
+order by avg_salary asc
+   limit 0, 1;
+   
 
--- 3-2) 복수행 연산자: in, not in, 비교 연산자 any, 비교 연산자 all
 
--- any 
+
+-- 3-2) 복수행 연산자: in, not in, 비교연산자any, 비교연산자all
+
+-- any 사용법
 -- 1. =any : in
--- 2. >any, >= anu : 최소값
--- 3. <any, <any : 최대값
--- 4. != any, <>any : not in
+-- 2. >any, >=any  : 최소값
+-- 3. <any, <=any  : 최대값
+-- 4. !=any, <>any : not in
 
 -- all 사용법
--- 1. = all : (x)
+-- 1. =all: (x)
 -- 2. >all, >=all : 최대값
 -- 3. <all, <=all : 최소값
--- 2. != all, <>all
+-- 4. !=all, <>all
 
--- 예제3) 현재 근무중인 사람 중 급여가 50000 이상인 직원의 이름과 급여를 출력하세요.(급여 내림차순)
--- 둘리  65000
--- 또치  70000
+-- 예제3: 현재, 급여가 50000 이상인 직원의 이름과 급여를 출력하세요.(급여 내림차순)
+-- 둘리 65000
+-- 또치 70000
 
 -- sol1) join
--- select e.first_name, s.salary from employees e, salaries s where e.emp_no = s.emp_no and s.to_date = '9999-01-01' and s.salary>= 50000 order by s.salary;
--- select * from employees, salaries where    ; 
-SELECT 
-    e.first_name, s.salary
-FROM
-    employees e,
-    salaries s
-WHERE
-    e.emp_no = s.emp_no
-        AND s.to_date = '9999-01-01'
-        AND (e.emp_no , s.salary) IN (SELECT 
-            e.emp_no, s.salary
-        FROM
-            salaries s,
-            employees e
-        WHERE
-            salary >= 50000
-                AND to_date = '9999-01-01');
-                
--- 문제4) : 현재 근무중인 인원 중 각 부서별로 최고 급여를 받고 있는 직원의 이름과 연봉 (복수 행 / 다중 컬럼)
+select a.first_name, b.salary
+  from employees a, salaries b
+ where a.emp_no = b.emp_no
+   and b.to_date = '9999-01-01'
+   and b.salary >= 50000
+order by b.salary asc;
 
--- so1) where subquery in
-SELECT 
-    e.first_name, demp.dept_no, dept.dept_name, s.salary
-FROM
-    employees e,
-    salaries s,
-    dept_emp demp,
-    departments dept
-WHERE
-    s.emp_no = e.emp_no
-        AND e.emp_no = demp.emp_no
-        AND demp.dept_no = dept.dept_no
-        AND demp.to_date = '9999-01-01'
-        AND s.to_date = '9999-01-01'
-        AND (demp.dept_no , s.salary) IN (SELECT 
-            demp.emp_no, MAX(s.salary)
-        FROM
-            salaries s,
-            dept_emp demp
-        WHERE
-            s.emp_no = demp.emp_no
-                AND s.to_date = '9999-01-01'
-                AND demp.to_date = '9999-01-01'
-        GROUP BY demp.dept_no);
+-- sol2) subquery
+
+ select a.first_name, b.salary
+   from employees a, salaries b
+  where a.emp_no = b.emp_no
+	and b.to_date = '9999-01-01'
+	and (a.emp_no, b.salary) =any (select a.emp_no, b.salary
+                                     from employees a, salaries b
+                                    where a.emp_no = b.emp_no
+                                      and b.to_date = '9999-01-01'
+									  and b.salary >= 50000)
+order by b.salary asc;
+
+ select a.first_name, b.salary
+   from employees a, salaries b
+  where a.emp_no = b.emp_no
+	and b.to_date = '9999-01-01'
+	and (a.emp_no, b.salary) in (select a.emp_no, b.salary
+                                     from employees a, salaries b
+                                    where a.emp_no = b.emp_no
+                                      and b.to_date = '9999-01-01'
+									  and b.salary >= 50000)
+order by b.salary asc;
+
+-- 문제4: 현재, 각 부서별로 최고 급여를 받고 있는 직원의 이름과 연봉을 출력하세요.
+-- 총무 둘리 1000
+-- 영업 또치 2000
+
+select a.dept_no, max(b.salary)
+  from dept_emp a, salaries b
+ where a.emp_no = b.emp_no
+   and a.to_date = '9999-01-01'
+   and b.to_date = '9999-01-01'
+group by a.dept_no;   
+   
+-- sol1) where절 subquery in, 복수행/복수컬럼
+select c.dept_name, a.first_name, d.salary
+  from employees a, dept_emp b, departments c, salaries d
+ where a.emp_no = b.emp_no
+   and b.dept_no = c.dept_no
+   and a.emp_no = d.emp_no
+   and b.to_date = '9999-01-01'
+   and d.to_date = '9999-01-01'
+   and (b.dept_no, d.salary) in (  select a.dept_no, max(b.salary)
+								     from dept_emp a, salaries b
+								    where a.emp_no = b.emp_no
+                                      and a.to_date = '9999-01-01'
+                                      and b.to_date = '9999-01-01'
+                                 group by a.dept_no);
+   
+
 
 -- sol2) from절 subquery, join
-SELECT 
-    e.first_name, demp.dept_no, dept.dept_name, s.salary
-FROM
-    employees e,
-    salaries s,
-    dept_emp demp,
-    departments dept,
-    (SELECT 
-            demp.emp_no, MAX(s.salary) as max_salary
-        FROM
-            salaries s,
-            dept_emp demp
-        WHERE
-            s.emp_no = demp.emp_no
-                AND s.to_date = '9999-01-01'
-                AND demp.to_date = '9999-01-01'
-        GROUP BY demp.dept_no) f
-WHERE
-    s.emp_no = e.emp_no
-        AND e.emp_no = demp.emp_no
-        AND demp.dept_no = dept.dept_no
-        and dept.dept_no = f.dept_no
-        AND demp.to_date = '9999-01-01'
-        AND s.to_date = '9999-01-01'
-        and s.salary = f.max_salary;
-
+select c.dept_name, a.first_name, d.salary
+  from employees a,
+	   dept_emp b,
+	   departments c,
+       salaries d,
+	   (  select a.dept_no, max(b.salary) as max_salary
+  	        from dept_emp a, salaries b
+		   where a.emp_no = b.emp_no
+             and a.to_date = '9999-01-01'
+             and b.to_date = '9999-01-01'
+        group by a.dept_no) e       
+ where a.emp_no = b.emp_no
+   and b.dept_no = c.dept_no
+   and a.emp_no = d.emp_no
+   and c.dept_no = e.dept_no
+   and b.to_date = '9999-01-01'
+   and d.to_date = '9999-01-01'
+   and d.salary = e.max_salary;
